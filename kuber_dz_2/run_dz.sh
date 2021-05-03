cc2253c016b47fb8cb88ae17adeb0c51812aa1f1
Gleb Danichev <gbdanichev@avito.ru>
2021-05-03 13:56:03 +0300

    Add jobs

--- kuber_dz_2/run_dz.sh
+++ kuber_dz_2/run_dz.sh
@@ -1,5 +1,12 @@
+echo "install Postgresql.."
+
 helm install glebpg \
---set postgresqlUsername=gleb,postgresqlPassword=secretpassword,postgresqlDatabase=kuber-dz-2 \
+--set persistenceEnabled=false,postgresqlUsername=gleb,postgresqlPassword=secretpassword,postgresqlDatabase=kuber-dz-2 \
 bitnami/postgresql
 
+helm upgrade kuber-dz-2-app chart
+
+#create table
+
+#arch.homework curls
 curl -s -X POST -d'{"username":"1sFT0I0m67","firstName":"6M1B1dtUN","lastName":"3oEHpLF79","email":"76Hdp","phone":"b4PYX72rE2"}'
